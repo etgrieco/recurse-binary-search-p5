7d2aaf6cf273df30ccb16ed881ab0add0b0bb45b
Elliott Grieco <elliott.grieco@gmail.com>
2025-02-21 23:36:09 -0500

    Attempt to handle font resources via resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   base: "/recurse-binary-search-p5/",
+  resolve: {
+    alias: {
+      "@fonts": path.resolve(__dirname, "./fonts"),
+    },
+  },
   plugins: [tailwindcss()],
 });
